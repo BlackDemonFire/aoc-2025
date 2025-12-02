554fe42779ec5af26fc850dff69a4b2d1123cb25
lucy <lucy@amogus.cloud>
2025-12-02 08:41:01 +0100

    add main.ts to run all days

---
+++ main.ts
@@ -0,0 +1,19 @@
+import { existsSync, readFileSync } from "node:fs";
+import { solveD01T1 } from "./d01t1.js";
+import { solveD01T2 } from "./d01t2.js";
+
+
+console.log("Running Day 01 with demo input:");
+
+const demoInput01 = readFileSync("d01-demo.txt", "utf-8");
+
+console.log("Day 01 - Task 1 (Demo):", solveD01T1(demoInput01));
+console.log("Day 01 - Task 2 (Demo):", solveD01T2(demoInput01));
+
+if (existsSync("d01.txt")) {
+    const input = readFileSync("d01.txt", "utf-8");
+    console.log("Day 01 - Task 1:", solveD01T1(input));
+    console.log("Day 01 - Task 2:", solveD01T2(input));
+} else {
+    console.log("No input file found for Day 01. (d01.txt)");
+}
